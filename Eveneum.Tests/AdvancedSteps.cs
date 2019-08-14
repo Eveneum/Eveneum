e21de35de2e2437622666a6a5607cc38d68d5987
Jakub Konecki <jkonecki@gmail.com>
2019-08-14 20:24:59 +0100

    Upgraded project files to SDK format

--- Eveneum.Tests/AdvancedSteps.cs
+++ Eveneum.Tests/AdvancedSteps.cs
@@ -22,7 +22,7 @@ namespace Eveneum.Tests
         {
             var events = new List<EventData>();
 
-            await (this.Context.EventStore as IAdvancedEventStore).LoadAllEvents(e => { events.AddRange(e); return Task.CompletedTask; });
+            await (this.Context.EventStore as IAdvancedEventStore).LoadAllEvents(e => { events.AddRange(e); return Task.FromResult(true); });
 
             ScenarioContext.Current.Set(events, "LoadAllEvents");
         }
@@ -32,7 +32,7 @@ namespace Eveneum.Tests
         {
             var events = new List<EventData>();
 
-            await(this.Context.EventStore as IAdvancedEventStore).LoadEvents(query, e => { events.AddRange(e); return Task.CompletedTask; });
+            await(this.Context.EventStore as IAdvancedEventStore).LoadEvents(query, e => { events.AddRange(e); return Task.FromResult(true); });
 
             ScenarioContext.Current.Set(events, "LoadAllEvents");
         }
