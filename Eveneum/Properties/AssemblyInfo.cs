f23aa96ceef2f565ba028879cfd2086e2534a316
Jakub Konecki <jkonecki@gmail.com>
2019-08-15 17:29:11 +0100

    Added assembly attributes for NuGet packaging

--- Eveneum/Properties/AssemblyInfo.cs
+++ Eveneum/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("Event Store backed by Cosmos DB")]
+[assembly: AssemblyCopyright("Copyright © Jakub Konecki 2018-19")]
 
 [assembly: InternalsVisibleTo("Eveneum.Tests")]
