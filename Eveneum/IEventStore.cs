9301f2238f93b3c351274329a7e2a9920f37bc94
Jakub Konecki <jkonecki@gmail.com>
2019-08-29 11:37:29 +0100

    Extracter specialized interfaces from IEventStore.

--- Eveneum/IEventStore.cs
+++ Eveneum/IEventStore.cs
@@ -3,15 +3,31 @@ using System.Threading.Tasks;
 
 namespace Eveneum
 {
-    public interface IEventStore
+    public interface IReadStream
     {
-        DeleteMode DeleteMode { get; set; }
         Task<Stream?> ReadStream(string streamId, CancellationToken cancellationToken = default);
         Task<Stream?> ReadStreamAsOfVersion(string streamId, ulong version, CancellationToken cancellationToken = default);
         Task<Stream?> ReadStreamIgnoringSnapshots(string streamId, CancellationToken cancellationToken = default);
+    }
+
+    public interface IWriteToStream
+    {
         Task WriteToStream(string streamId, EventData[] events, ulong? expectedVersion = null, object metadata = null, CancellationToken cancellationToken = default);
+    }
+
+    public interface IDeleteStream
+    {
+        DeleteMode DeleteMode { get; set; }
         Task DeleteStream(string streamId, ulong expectedVersion, CancellationToken cancellationToken = default);
+    }
+
+    public interface IManageSnapshots
+    {
         Task CreateSnapshot(string streamId, ulong version, object snapshot, object metadata = null, bool deleteOlderSnapshots = false, CancellationToken cancellationToken = default);
         Task DeleteSnapshots(string streamId, ulong olderThanVersion, CancellationToken cancellationToken = default);
+    }
+
+    public interface IEventStore : IReadStream, IWriteToStream, IDeleteStream, IManageSnapshots
+    {
     }
 }
